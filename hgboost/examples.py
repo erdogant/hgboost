f1b51b3ab1cf2df9f2033bb855ae5e2f6acf7738
Erdogan Taskesen <erdogant@gmail.com>
2020-09-04 12:36:18 +0200

    update

--- hgboost/examples.py
+++ hgboost/examples.py
@@ -155,22 +155,26 @@ iris = datasets.load_iris()
 X = pd.DataFrame(iris.data, columns=iris['feature_names'])
 y = iris.target
 
-hgb = hgboost(method='xgb_clf', max_eval=100, eval_metric='auc')
-results = hgb.fit(X, y==1)
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+results = hgb.xgboost(X, y, pos_label=0)
 
 # Plot
 hgb.plot_params()
 hgb.plot()
 hgb.treeplot()
 hgb.plot_validation()
+hgb.plot_cv()
 
 # %% CLASSIFICATION MULTI-CLASS #####
 iris = datasets.load_iris()
 X = pd.DataFrame(iris.data, columns=iris['feature_names'])
 y = iris.target
 
-hgb = hgboost(method='xgb_clf_multi', max_eval=100, eval_metric='mlogloss')
-results = hgb.fit(X, y)
-hgb.treeplot()
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+results = hgb.xgboost(X, y, method="xgb_clf_multi")
+
+hgb.plot_params()
 hgb.plot()
+hgb.treeplot()
 hgb.plot_validation()
+hgb.plot_cv()
