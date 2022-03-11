3dbb1c04d843d379cde91ccf26ddca07b8c8cbc8
erdogant <erdogant@gmail.com>
2022-03-11 14:02:55 +0100

    update url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
      description="hgboost is a python package for hyperparameter optimization for xgboost, catboost and lightboost for both classification and regression tasks.",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/erdogant/hgboost",
+     url="https://erdogant.github.io/hgboost",
 	 download_url = 'https://github.com/erdogant/hgboost/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
