7e8db633c091972e0ff12163c88f7597ac465370
erdogant <erdogant@gmail.com>
2026-04-06 20:21:17 +0200

    cleaning

--- hgboost/shapplot.py
+++ hgboost/shapplot.py
@@ -5,15 +5,6 @@
  INPUT:
    model:          Trained model
 
- OPTIONAL
-
-   verbose:        Integer or String
-                   NOTHING=0
-                   ERROR=1
-                   WARN=2
-                   INFO=3
-                   DEBUG=4
-
  OUTPUT
 	output
 
